21428fae5527fe2821fc7bf4b1227937e45a5b2b
Viktória Sanislová <viktoria.sanislova@student.tuke.sk>
2021-11-11 21:29:16 +0100

    Update main.c

--- ps3/main.c
+++ ps3/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <time.h>
 #include "hangman.h"
-#include<string.h> 
-
 
 int main(){
+    srand(time(NULL));
+    char secret[30];
+    get_word(secret);
+    hangman(secret);
+    
     return 0;
 }
