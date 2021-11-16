19489b99e97d1fbbb4a498fa9817d3ac9b2d8b12
Viktória Sanislová <viktoria.sanislova@student.tuke.sk>
2021-11-16 22:46:19 +0100

    Update main.c

--- ps4/main.c
+++ ps4/main.c
@@ -6,11 +6,58 @@
 
 
 int main(){
-srand(time(NULL));
+    srand(time(NULL));
     int rows=4;
     int columns=6;
-    char field[rows][columns];
-    game_field(rows,columns,field);
-return 0;
-}
+    char fidesz[rows][columns];
+    bool ellenor;
+    generator(rows,columns,fidesz);
 
+    do{
+        down_possible(rows,columns,fidesz);
+  
+        ellenor=true;
+                        //innentol csak kiiratas a while vegeig
+        for (int i=0; i<rows; i++){
+            printf ("%c", ' ');
+            printf ("%i", i+1);
+            printf ("%c", ' ');        printf ("%c", '|');printf ("%c", ' ');
+            
+            for(int j=0; j<columns ; j++){
+                
+                    if(fidesz[0][j]!=fidesz[i][j]){             //megegyezik-e az 1. (0.) elemmel a tobbi az oszlopban, azert a legelsovel hasonlitjuk ossze, hogy csak akkor legyen true, ha az egesz sor tele van (amig nincs tele addig az elso sorban szokoz van, tehat nem egyezik meg, ha meg teljesen ures akkor meg mar ujra jo)
+                        ellenor=false;                          //helyes sorrend ellenorzese
+                    }
+                
+                printf ("%c", fidesz[i][j]);
+                printf ("%c", ' ');
+                printf ("%c", '|');
+                printf ("%c", ' ');
+            }
+            printf("\n");
+        }
+            printf("   ");
+        for(int k=0;k<columns;k++){
+            printf ("%c", '-');
+            printf ("%c", '-');
+            printf ("%c", '-');
+            printf ("%c", ' ');
+        }
+        
+            printf("\n");
+            printf ("   %c", ' ');
+            
+        for(int k=1;k<columns+1;k++){
+            printf ("%c", ' ');
+            printf ("%i", k);
+            printf (" %c", ' ');
+            
+        }
+        
+        printf("\n");
+
+    }while(ellenor==false);
+
+    printf("No konecne! trvalo to dlho ty tupak :)");
+    return 0;
+}
