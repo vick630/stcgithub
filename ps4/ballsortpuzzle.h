11f318b72e2de2aaafe9d89305e035479b27cb2b
Viktória Sanislová <viktoria.sanislova@student.tuke.sk>
2021-11-18 23:30:28 +0100

    Update ballsortpuzzle.h

--- ps4/ballsortpuzzle.h
+++ ps4/ballsortpuzzle.h
@@ -1,10 +1,24 @@
+#include <stdbool.h>
+
+#define BLANK ' '
+
 /**
  * Function draw game field
  * @param rows count of rows
  * @param columns count of columns
  * @param field hold 2d array of characters in game
  */
-void generator(const int rows, const int columns, char fidesz[rows][columns]);
+void game_field(const int rows, const int columns, char field[rows][columns]);
+
+
+/**
+ * Check fields rows if there are same characters
+ * @param rows count of rows
+ * @param columns count of columns
+ * @param field hold 2d array of characters in game
+ * @return true, if columns are complete; false otherwise
+ */
+bool check(const int rows, const int columns, char field[rows][columns]);
 
 
 
@@ -21,8 +35,18 @@ void generator(const int rows, const int columns, char fidesz[rows][columns]);
 void down_possible(const int rows, const int columns, char field[rows][columns], int x, int y);
 
 
+/**
+ * Function returns 2D array of randomly generated characters
+ * There must be two blank columns and other should be full of characters without '\0' or blank spaces
+ * @param rows count of rows
+ * @param columns count of columns
+ * @param field hold 2d array of characters in game
+ */
+void generator(const int rows, const int columns, char field[rows][columns]);
+
 
 /**
  * Starts interactive ball sort puzzle game
  */
 void ball_sort_puzzle();
+
