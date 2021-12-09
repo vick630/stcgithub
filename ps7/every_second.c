6d51da578fbbb93b44efbe0476780129c975c420
Viktória Sanislová <viktoria.sanislova@student.tuke.sk>
2021-12-09 15:10:26 +0100

    Update every_second.c

--- ps7/every_second.c
+++ ps7/every_second.c
@@ -2,8 +2,8 @@
 
     int main(int argc, char* argv[])
     {
-        FILE *subor=fopen (argv[1],"r+");
         FILE *suboor=fopen (argv[2],"w");
+        FILE *subor=fopen (argv[1],"r+");
         int p=0;
         int a=0;
         char ok=fgetc(subor);
@@ -13,38 +13,27 @@
                     if(ok==84)
                     { ok=fgetc(subor);
                         if(ok==65)
-                        {
-
-                            ok=fgetc(subor);
+                        { ok=fgetc(subor);
                             if(ok==82)
-                            {
-
-                                ok=fgetc(subor);
+                            { ok=fgetc(subor);
                                 if(ok==84)
                                 {
-                                    ok=fgetc(subor);
+                                  ok=fgetc(subor);
                                     while ( ok != EOF )
                                     {
-
                                         if (ok==83)
-                                        {
-
-                                            a=a+1;
+                                        {  a=a+1;
                                             ok=fgetc(subor);
                                             if (ok==84)
-                                            {
-                                                a=a+1;
+                                            {   a=a+1;
                                                 ok=fgetc(subor);
                                                 if (ok==79)
-                                                {
-
-                                                    a++;
+                                                {   a=a+1;
                                                     ok=fgetc(subor);
                                                     if (ok==80)
                                                     {
 
-                                                        break;
-                                                        
+                                                        break;   
                                                     }
 
                                                 }
@@ -52,37 +41,24 @@
                                             }
 
                                         }
-
-
-                                        if (p%2 ==0 && p>3)
+                                        if (p>3 && p%2 ==0)
                                         {
-
                                             fputc(32, suboor);
-
+                                        }
+                                        if (p%2==0 && a==1 )
+                                        {
+                                            fputc(83, suboor);
+                                        }
+                                        if (p%2==0 && a==2 )
+                                        {fputc(84, suboor);
+                                        fputc(83, suboor);
+                                            
                                         }
 
-
-                                        if (a==1 && p%2==0)
-                                        {
-
-                                            fputc(83, suboor);
-
-                                        }
-
-                                        if (a==2 && p%2==0)
-                                        {
-
-                                            fputc(83, suboor);
+                                        if (p%2==0 && a==3)
+                                        {   fputc(79, suboor);
                                             fputc(84, suboor);
-
-                                        }
-
-                                        if (a==3 && p%2==0)
-                                        {
-
                                             fputc(83, suboor);
-                                            fputc(84, suboor);
-                                            fputc(79, suboor);
 
                                         }
                                         a=0;
